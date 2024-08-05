347209a34a52d0bfec64989b1239fc88766a661e
Sigrsig <sigriur.oesp.green.sigurardottir@sap.com>
2024-08-05 15:27:19 +0000

    New background added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Home.css";
-import wallpaper from "../images/wallpaper.jpg";
 import Nav from "./Nav";
 import Event from "./pages/Event";
 import { Outlet } from "react-router-dom";
